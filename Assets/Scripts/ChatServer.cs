7451ab4b7216637c6e1cced08dca43713655225d
baercr <baercr@mail.uc.edu>
2023-09-23 18:55:08 -0400

    Update ChatServer.cs

--- Assets/Scripts/ChatServer.cs
+++ Assets/Scripts/ChatServer.cs
@@ -6,11 +6,48 @@ using Unity.Netcode;
 public class ChatServer : NetworkBehaviour
 {
     public ChatUi chatUi;
+    const ulong SYSTEM_ID = ulong.MaxValue;
+    private ulong[] dmClientIds = new ulong[2];
 
-    void Start()
-    {
+    void Start() {
         chatUi.printEnteredText = false;
         chatUi.MessageEntered += OnChatUiMessageEntered;
+
+        if (IsServer) {
+            NetworkManager.OnClientConnectedCallback += ServerOnClientConnected;
+            if (IsHost) 
+            {
+                DisplayMessageLocally(SYSTEM_ID, $"You are the host AND client {NetworkManager.LocalClientId}");
+            } else {
+                DisplayMessageLocally(SYSTEM_ID, "You are the server");
+            }
+        } else {
+            DisplayMessageLocally(SYSTEM_ID, $"You are client {NetworkManager.LocalClientId}");
+        }
+    }
+
+    private void ServerOnClientConnected(ulong clientId)
+    {
+        /*ServerSendDirectMessage(
+            $"I ({NetworkManager.LocalClientId}) see you ({clientId}) have connected to the server, well done",
+            NetworkManager.LocalClientId,
+            clientId);*/
+    }
+
+    private void DisplayMessageLocally(ulong from, string message) {
+        string fromStr = $"Player {from}";
+        Color textColor = chatUi.defaultTextColor;
+
+        if (from == NetworkManager.LocalClientId)
+        {
+            fromStr = "you";
+            textColor = Color.magenta;
+        } else if(from == SYSTEM_ID)  {
+            fromStr = "SYS";
+            textColor = Color.green;
+        }
+
+        chatUi.addEntry(fromStr, message, textColor);
     }
 
     private void OnChatUiMessageEntered(string message)
@@ -21,13 +58,36 @@ public class ChatServer : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     public void SendChatMessageServerRpc(string message, ServerRpcParams serverRpcParams = default) 
     {
-        ReceiveChatMessageClientRpc(message, serverRpcParams.Receive.SenderClientId);
+        if (message.StartsWith("@")) {
+            string[] parts = message.Split(" ");
+            string clientIdStr = parts[0].Replace("@", "");
+            ulong toClientId = ulong.Parse(clientIdStr);
+
+            ServerSendDirectMessage(message, serverRpcParams.Receive.SenderClientId, toClientId);   
+        } else {
+            ReceiveChatMessageClientRpc(message, serverRpcParams.Receive.SenderClientId);
+        }
+
     }
 
     [ClientRpc]
     public void ReceiveChatMessageClientRpc(string message, ulong from, ClientRpcParams clientRpcParams = default) 
     {
-        chatUi.addEntry($"Player {from}", message);
+        DisplayMessageLocally(from, message);
     }
 
+
+    private void ServerSendDirectMessage(string message, ulong from, ulong to) {
+        dmClientIds[0] = from;
+        dmClientIds[1] = to;
+
+        ClientRpcParams rpcParams = default;
+        rpcParams.Send.TargetClientIds = dmClientIds;
+
+        /*clientIds[0] = from;
+        ReceiveChatMessageClientRpc($"<whisper> {message}", from, rpcParams);
+
+        clientIds[0] = to;*/
+        ReceiveChatMessageClientRpc($"<whisper> {message}", from, rpcParams);
+    }
 }
